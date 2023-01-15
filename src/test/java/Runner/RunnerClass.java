1c64ccf7109029250cff0f54db88dd219338fdf6
farukyucel5 <117035308+farukyucel5@users.noreply.github.com>
2023-01-15 18:17:46 +0300

    15.01.2023 runner update

--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -3,25 +3,22 @@ package Runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-public class RunnerClass {
-    @CucumberOptions(
 
-            features = {"src/test/java"}, //it will run all test case features but the code lines below run specific features
-            //features={"src/test/java/Features/SearchTest.feature"},
+@CucumberOptions(
 
-            glue = {"Steps"},
+        features = {"src/test/java"}, //it will run all test case features but the code lines below run specific features
+        //features={"src/test/java/Features/SearchTest.feature"},
 
-            plugin = { "pretty",
-                    "html:target/site/cucumber-pretty",
-                    "json:target/cucumber.json","html:target/cucumber-html-report","usage:target/cucumber-usage.json","" +
-                    "junit:target/cucumber-results.xml" },
-            dryRun = false
-    )
+        glue = {"Steps"},
 
+        plugin = { "pretty",
+                "html:target/site/cucumber-pretty",
+                "json:target/cucumber.json","html:target/cucumber-html-report","usage:target/cucumber-usage.json","" +
+                "junit:target/cucumber-results.xml" },
+        dryRun = false
+)
 
+public class RunnerClass extends AbstractTestNGCucumberTests  {
 
-    public static class RunnerGenel extends AbstractTestNGCucumberTests {
-
-    }
 
 }
